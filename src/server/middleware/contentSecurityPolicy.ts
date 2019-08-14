56b927f607c381711b69ab64d7c487b4cd9fd703
Ross <ross_patton@protonmail.com>
2019-08-14 18:10:21 -0400

    CSP with nonce working. god damn it.

--- src/server/middleware/contentSecurityPolicy.ts
+++ src/server/middleware/contentSecurityPolicy.ts
@@ -4,81 +4,44 @@ import koaHelmet, { KoaHelmetContentSecurityPolicyConfiguration } from 'koa-helm
 // only allow assets served from our origin or a trusted cdn
 // required nonces for any inline scripts and SRI for cdns to verify integrity
 export const contentSecurityPolicyMiddleware = async (app: Koa) => {
-  const directives = __DEV__ ?
-    ["'self'", '0.0.0.0:*', '127.0.0.1:*', 'localhost:*'] :
-    ["'self'"];
+  app.use((ctx, next) => {
+    const directives = __DEV__ ?
+      ["'self'", '0.0.0.0:*', '127.0.0.1:*', 'localhost:*'] :
+      ["'self'"];
 
-  const cspOpts: KoaHelmetContentSecurityPolicyConfiguration = {
-    directives: {
-      defaultSrc: directives,
-      frameAncestors: ["'none'"],
-      imgSrc: ["'self'", 'picsum.photos', 'via.placeholder.com'],
-      objectSrc: ["'none'"],
-      sandbox: [
-        'allow-forms',
-        'allow-same-origin',
-        'allow-scripts',
-      ],
-      // TODO cant use inline css in svgs either, at least in ff
-      // unsafe inline just here for now until i can redo the svgs
-      styleSrc: [...directives, "'unsafe-inline'"],
-      scriptSrc: [
-        ...directives,
-        "'unsafe-inline'",
-        'ajax.googleapis.com',
-        // @ts-ignore
-        // `'nonce-${ctx.state.locals.nonce}'`,
-      ],
-      upgradeInsecureRequests: true,
-    },
-    reportOnly: true,
-  };
-
-  if (__DEBUG__ || __PROD__) {
-    cspOpts.reportOnly = true;
-    cspOpts.directives = {
-      ...cspOpts.directives,
-      reportUri: '/report-violation',
+    const cspOpts: KoaHelmetContentSecurityPolicyConfiguration = {
+      directives: {
+        defaultSrc: directives,
+        frameAncestors: ["'none'"],
+        imgSrc: ["'self'", 'picsum.photos', 'via.placeholder.com'],
+        objectSrc: ["'none'"],
+        sandbox: [
+          'allow-forms',
+          'allow-same-origin',
+          'allow-scripts',
+        ],
+        // TODO cant use inline css in svgs either, at least in ff
+        // unsafe inline just here for now until i can redo the svgs
+        styleSrc: [...directives, "'unsafe-inline'"],
+        scriptSrc: [
+          ...directives,
+          "'unsafe-inline'",
+          'ajax.googleapis.com',
+          `'nonce-${ctx.state.locals.nonce}'`,
+        ],
+        upgradeInsecureRequests: true,
+      },
+      reportOnly: true,
     };
-  }
 
-  app.use(koaHelmet.contentSecurityPolicy(cspOpts));
+    if (__DEBUG__ || __PROD__) {
+      cspOpts.reportOnly = true;
+      cspOpts.directives = {
+        ...cspOpts.directives,
+        reportUri: '/report-violation',
+      };
+    }
+
+    return koaHelmet.contentSecurityPolicy(cspOpts)(ctx, next);
+  });
 };
-
-// await app.use(async (ctx, next) => {
-//   console.log('ctx.state.locals => ', ctx.state.locals);
-//   const directives = __DEV__ ?
-//     ["'self'", '0.0.0.0:*', '127.0.0.1:*', 'localhost:*'] :
-//     ["'self'"];
-
-//   const cspOpts: KoaHelmetContentSecurityPolicyConfiguration = {
-//     directives: {
-//       defaultSrc: directives,
-//       frameAncestors: ["'none'"],
-//       objectSrc: ["'none'"],
-//       sandbox: [
-//         'allow-forms',
-//         'allow-same-origin',
-//         'allow-scripts',
-//       ],
-//       styleSrc: directives,
-//       scriptSrc: [
-//         'ajax.googleapis.com',
-//         `'nonce-${ctx.state.locals.nonce}'`,
-//       ],
-//       upgradeInsecureRequests: true,
-//     },
-//     reportOnly: false,
-//   };
-
-//   if (__DEBUG__ || __PROD__) {
-//     cspOpts.reportOnly = true;
-//     cspOpts.directives = {
-//       ...cspOpts.directives,
-//       reportUri: '/report-violation',
-//     };
-//   }
-
-//   await koaHelmet.contentSecurityPolicy(cspOpts);
-//   await next();
-// });
