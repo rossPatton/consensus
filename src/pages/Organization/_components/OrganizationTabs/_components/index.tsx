8ade0fd5633ecec8a8788e0b687870b9ad318050
Ross <ross_patton@protonmail.com>
2019-10-09 19:24:56 -0400

    fix org tabs so they work regardless of params

--- src/pages/Organization/_components/OrganizationTabs/_components/index.tsx
+++ src/pages/Organization/_components/OrganizationTabs/_components/index.tsx
@@ -5,10 +5,12 @@ import {tProps} from './_types';
 
 export const Tab = memo((props: tProps) => {
   const { match, subRoute = '' } = props;
-  const { country, city, region, slug, section } = match.params;
-  const to = `/org/${country}/${region}/${city}/${slug}/${subRoute}`;
-  const cx = 'ttCap dBl p2 pL3 pR3';
+  const { country, city, id, region, slug, section } = match.params;
+  const to = id
+    ? `/org/${id}/${subRoute}`
+    : `/org/${country}/${region}/${city}/${slug}/${subRoute}`;
 
+  const cx = 'ttCap dBl p2 pL3 pR3';
   if (section === subRoute) {
     return (
       <span className={`${cx} bgGrey2`}>
