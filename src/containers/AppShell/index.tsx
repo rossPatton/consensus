3dd74bac8e539336fb16f0b6dcac52f47a4cf3c0
Ross <ross_patton@protonmail.com>
2020-04-14 21:12:48 -0400

    add session provider

--- src/containers/AppShell/index.tsx
+++ src/containers/AppShell/index.tsx
@@ -3,20 +3,22 @@ import {HelmetProvider} from 'react-helmet-async';
 import {Switch} from 'react-router-dom';
 
 import {RouteWithSubRoutes} from '../../components';
-import {MatchMediaProvider} from '../../context';
+import {MatchMediaProvider, SessionProvider} from '../../context';
 import {routes} from '../../routes';
 
 const AppShell = memo(() => (
   <HelmetProvider context={{}}>
     <MatchMediaProvider>
-      <Switch>
-        {routes.map((route: tRoute, i) => (
-          <RouteWithSubRoutes
-            key={i}
-            {...route}
-          />
-        ))}
-      </Switch>
+      <SessionProvider>
+        <Switch>
+          {routes.map((route: tRoute, i) => (
+            <RouteWithSubRoutes
+              key={i}
+              {...route}
+            />
+          ))}
+        </Switch>
+      </SessionProvider>
     </MatchMediaProvider>
   </HelmetProvider>
 ));
--- src/context/index.ts
+++ src/context/index.ts
@@ -3,3 +3,7 @@ import loadable from '@loadable/component';
 export const MatchMediaProvider = loadable(() =>
   import(/* webpackChunkName: "MatchMediaProvider" */'./MatchMediaProvider'),
 );
+
+export const SessionProvider = loadable(() =>
+  import(/* webpackChunkName: "SessionProvider" */'./SessionProvider'),
+);
