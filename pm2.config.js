29a0630bb7475e09a479fba8276a4d8a080ccd1a
Ross <ross_patton@protonmail.com>
2020-04-16 22:15:06 -0400

    remove a few more unused webpack/babel plugins/loaders, and make some minor changes to pm2, docker-compose files

--- pm2.config.js
+++ pm2.config.js
@@ -1,27 +1,23 @@
-const { NODE_ENV } = process.env;
+const { DEBUG, NODE_ENV } = process.env;
+
+// enables cluster mode basically, up to max number of available cores
+let clusterMode = {};
+if (NODE_ENV === 'production') {
+  clusterMode = {
+    instances: 'max',
+    exec_mode: 'cluster'
+  };
+}
 
 module.exports = {
   apps: [{
     name: 'consensus',
     script: './dist/server.js',
     // options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: ['--max-old-space-size=8192', '--inspect=0.0.0.0'],
-    // enables cluster mode basically, up to max number of available cores
-    // instances: 'max',
-    // exec_mode: 'cluster',
+    node_args: ['--max-old-space-size=8192', '--inspect=9229'],
     autorestart: NODE_ENV === 'production',
     watch: NODE_ENV === 'development',
     max_memory_restart: '1G',
+    ...clusterMode
   }],
-
-  // deploy: {
-  //   production: {
-  //     user: 'node',
-  //     host: '212.83.163.1',
-  //     ref: 'origin/master',
-  //     repo: 'git@github.com:repo.git',
-  //     path: '/var/www/production',
-  //     'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
-  //   },
-  // },
 };
