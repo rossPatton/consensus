9907940c1071594e31fbef6877e5184a3034d45a
Ross <ross_patton@protonmail.com>
2021-02-09 21:19:14 -0500

    add isPublished column to track if meeting has ever been published. will help with UI

---
+++ src/server/db/migrations/02092020_0_meetings_isPublished.ts
@@ -0,0 +1,13 @@
+import Knex from 'knex';
+
+export const up = async (knex: Knex) => {
+  await knex.schema.table('meetings', t => {
+    t.boolean('isPublished').defaultTo(false);
+  });
+};
+
+export const down = async (knex: Knex) => {
+  await knex.schema.table('meetings', t => {
+    t.dropColumn('isPublished');
+  });
+};
